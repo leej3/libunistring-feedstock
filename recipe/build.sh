b0848528c6e3d83885230efb219f44074147f09a
Paul Yim <paul.j.yim@gmail.com>
2021-02-17 03:24:14 +0000

    Skip 'make check' when bootstrapping

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
 ./configure --disable-dependency-tracking --disable-silent-rules --prefix=${PREFIX}
+
 make
+
 if [ ${target_platform} == linux-ppc64le ]; then
   make check || true
 else
-  make check
+  if [[ ! $BOOTSTRAPPING == yes ]]; then
+    make check
+  fi
 fi
+
 make install
